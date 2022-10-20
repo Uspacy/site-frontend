44ee2e29778ec2527da0e85afad7a20b8652e2c7
SlavaPachevsky <savaukr17101980@gmail.com>
2022-10-20 18:22:18 +0300

    modalConsent

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,28 @@
 function main() {
 	const modalButtons = document.querySelectorAll('.js-open-modal');
 	const closeButton = document.querySelector('.js-modal-close');
+	const backButton = document.querySelector('.js-modal-back');
 	const modalElem = document.querySelector('.js-modal');
 	const overlay = document.querySelector('.js-overlay');
 
-	modalButtons.forEach(function (item) {
-		item.addEventListener('click', function (e) {
-			e.preventDefault();
-			modalElem.classList.add('active');
-			overlay.classList.add('active');
-		});
-	});
-
-	closeButton.addEventListener('click', function (e) {
+	function openModal(e) {
+		e.preventDefault();
+		modalElem.classList.add('active');
+		overlay.classList.add('active');
+	}
+	function closeModal(e) {
+		e.preventDefault();
 		modalElem.classList.remove('active');
 		overlay.classList.remove('active');
+	}
+
+	modalButtons.forEach(function (item) {
+		item.addEventListener('click', openModal);
 	});
+
+	closeButton.addEventListener('click', closeModal);
+	overlay.addEventListener('click', closeModal);
+	backButton.addEventListener('click', closeModal);
 }
 
 document.addEventListener('DOMContentLoaded', main);
