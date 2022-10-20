51a7986c8ccda9bb2167c6b816ee835efe776dde
SlavaPachevsky <savaukr17101980@gmail.com>
2022-10-20 18:03:36 +0300

    close modal

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,21 @@
 function main() {
 	const modalButtons = document.querySelectorAll('.js-open-modal');
 	const closeButton = document.querySelector('.js-modal-close');
-	// eslint-disable-next-line no-console
-	console.log('modalButtons:', modalButtons);
+	const modalElem = document.querySelector('.js-modal');
+	const overlay = document.querySelector('.js-overlay');
 
 	modalButtons.forEach(function (item) {
 		item.addEventListener('click', function (e) {
 			e.preventDefault();
-			const modalElem = document.querySelector('.js-modal');
-			const overlay = document.querySelector('.js-overlay');
 			modalElem.classList.add('active');
 			overlay.classList.add('active');
 		});
 	});
 
-	if (closeButton) {
-		alert(closeButton);
-	}
+	closeButton.addEventListener('click', function (e) {
+		modalElem.classList.remove('active');
+		overlay.classList.remove('active');
+	});
 }
+
 document.addEventListener('DOMContentLoaded', main);
