a9cee2582d1bf4c58df0966b9df3b0f9763e67be
SlavaPachevsky <savaukr17101980@gmail.com>
2022-10-20 17:50:53 +0300

    modal dont clode yet

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,21 @@
-// eslint-disable-next-line no-console
-console.log('Init');
+function main() {
+	const modalButtons = document.querySelectorAll('.js-open-modal');
+	const closeButton = document.querySelector('.js-modal-close');
+	// eslint-disable-next-line no-console
+	console.log('modalButtons:', modalButtons);
+
+	modalButtons.forEach(function (item) {
+		item.addEventListener('click', function (e) {
+			e.preventDefault();
+			const modalElem = document.querySelector('.js-modal');
+			const overlay = document.querySelector('.js-overlay');
+			modalElem.classList.add('active');
+			overlay.classList.add('active');
+		});
+	});
+
+	if (closeButton) {
+		alert(closeButton);
+	}
+}
+document.addEventListener('DOMContentLoaded', main);
