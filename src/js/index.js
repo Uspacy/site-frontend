c8ece2582e3ab1a9e5099035253d461fcd23ec6b
SlavaPachevsky <savaukr17101980@gmail.com>
2022-10-26 20:03:17 +0300

    disabled corsa

--- src/js/index.js
+++ src/js/index.js
@@ -182,7 +182,7 @@ function main() {
 				return;
 			}
 
-			const url = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSePNhSya-JPbAQHCN91X95hJRVfaitw8x25yjfQM5Qs_vH47w/formResponse';
+			const url = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSePNhSya-JPbAQHCN91X95hJRVfaitw8x25yjfQM5Qs_vH47w/formResponse?&submit=Submit';
 			const formData = new FormData();
 			formData.append('entry.568783889', email.value);
 			const response = await fetch(url, {
@@ -190,7 +190,8 @@ function main() {
 				body: formData,
 				mode: 'no-cors',
 			});
-			if (response.ok) {
+			// if (response.ok)
+			if (response) {
 				// eslint-disable-next-line no-invalid-this
 				if (this.classList.contains('jsModalForm')) {
 					showHideModalSuccess();
